8a280d41b9c0f1e544e7ac0aceec9bbcac90a1bc
Jacob Bednarz <n.a.r.z.y@live.com>
2011-10-29 03:44:25 +1100

    Modified $array_uri to be more flexible with multiple parameters
    Updated $array_uri to now include a 'vars' key which holds an array of parameters that can be used similar to query strings would -- just cleaner.

--- public/index.php
+++ public/index.php
@@ -45,13 +45,12 @@ $url = str_replace('/zeavo_framework', '', $url);
 	
 /**
  * Grab the URL and break it into more usable pieces 
- * TODO: Maybe rework this to be a bit cleaner
  */
 $array_tmp_uri = preg_split('[\\/]', $url, -1, PREG_SPLIT_NO_EMPTY);
 
 @$array_uri['controller'] = $array_tmp_uri[0];
 @$array_uri['method'] = $array_tmp_uri[1]; 
-@$array_uri['var'] = $array_tmp_uri[2]; 
+@$array_uri['vars'] = array_slice($array_tmp_uri, 2);
 	
 /**
  * Include the application bootstrap. Magic happens here! 
