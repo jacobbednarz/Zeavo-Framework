16c9e546ad457b284115a2b572de18cd3dfda4c9
Jacob Bednarz <jacob.bednarz@gmail.com>
2012-01-05 11:15:50 +1100

    Removed unnecessary lines for example
    Just extra clutter in an example file

--- application/views/example_view.php
+++ application/views/example_view.php
@@ -8,12 +8,6 @@
 <body>
     <h1><?php echo $pageHeader; ?></h1>
     <p><?php echo $welcomeParagraph; ?></p>
-    <p>This file is <code>example_view.php</code> from <code>application/views</code> directory. Additional variables passed to this file by creating another method and extracting the variable.</p>
-    <pre>
-function myMethod($queryString = '') {
-    $data['queryString'] = $queryString;
-    $this->loadView('view_file', $data);
-}
-    </pre>
+    <p>This file is <code>example_view.php</code> from <code>application/views</code> directory.</p>
 </body>
 </html>
